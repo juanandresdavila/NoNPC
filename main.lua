ebf49f5a2e7ec4909684b43b111cd7ef2f11e201
ZAD4YTV <zad4ytv@gmail.com>
2020-12-30 17:00:34 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,6 +2,7 @@ Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0) -- Prevent crashing.
 
+		-- Stop Spawn
 		SetCreateRandomCops(false)
 		SetCreateRandomCopsNotOnScenarios(false)
 		SetCreateRandomCopsOnScenarios(false)
@@ -13,4 +14,8 @@ Citizen.CreateThread(function()
 		SetScenarioPedDensityMultiplierThisFrame(0.0, 0.0)
 		SetParkedVehicleDensityMultiplierThisFrame(0.0)
 
+		-- Clear NPC
+		local x,y,z = table.unpack(GetEntityCoords(PlayerPedId()))
+		ClearAreaOfVehicles(x, y, z, 1000, false, false, false, false, false)
+		RemoveVehiclesFromGeneratorsInArea(x - 500.0, y - 500.0, z - 500.0, x + 500.0, y + 500.0, z + 500.0);
 end)
