ea0c8a65279e9b9f8a9dac0cdde540306a2d3f8f
Juan Andres Davila <zad4y@protonmail.com>
2021-06-06 01:26:34 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -18,4 +18,5 @@ Citizen.CreateThread(function()
 		local x,y,z = table.unpack(GetEntityCoords(PlayerPedId()))
 		ClearAreaOfVehicles(x, y, z, 1000, false, false, false, false, false)
 		RemoveVehiclesFromGeneratorsInArea(x - 500.0, y - 500.0, z - 500.0, x + 500.0, y + 500.0, z + 500.0);
+    end
 end)
